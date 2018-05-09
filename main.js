9c808a65674077d3178fe875a2f179f9997dfee9
Daniel S <danielksstone@gmail.com>
2018-05-08 17:37:44 -0700

    added a photo and trying to set up matching functionality

--- main.js
+++ main.js
@@ -3,4 +3,17 @@ $(document).ready(function () {
         $('#sidebar').toggleClass('active');
         $(this).toggleClass('active');
     });
+
 });
+
+function pairCheck (card1, card2) {
+    if (card1 === card2) {
+        console.log("match!")
+    } else {
+        console.log("try again")
+    }
+}
+
+$(".back").click(function() {
+    $(".card").css("display", "none")
+})
