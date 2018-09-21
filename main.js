0c727857995d2cde1e9a0ec3ff1abe08d94d4c86
Daniel S <danielksstone@gmail.com>
2018-09-20 17:17:44 -0700

    updated some sizing issues

--- main.js
+++ main.js
@@ -79,16 +79,12 @@ function arrayRandomizer (anyArray) {
 // function to dynamically create divs for the game depending on the length i made using the splitting array's function
 function generateDivs () {        
     for (var i=0;i<randomizedArray.length;i++) {
-    var myCol = $('<div class="columns col-xs-3"></div>');
-    var myPanel = $('<div class="card card-outline-info"></div>');
-    var myCardBlock = $('<div class="card-block"></div>');
+    var myCol = $('<div class="card-block col-xs-3"></div>');
     // using the randomizedArray to input names
     var myCardTitleFace = $(`<div class="card-title face"><img class="pictures card-${randomizedArray[i].name}" src="${randomizedArray[i].image}"></div>`);
-    var myCardTitleBack = $('<div class="card-title back"><img class="card-img" src="images/cardback.jpg"></div>');
-    myCardTitleFace.appendTo(myCardBlock);
-    myCardTitleBack.appendTo(myCardBlock);
-    myCardBlock.appendTo(myPanel);
-    myPanel.appendTo(myCol);
+    var myCardTitleBack = $('<div class="card-title back"><img class="cardBack" src="images/cardback.jpg"></div>');
+    myCardTitleFace.appendTo(myCol);
+    myCardTitleBack.appendTo(myCol);
     myCol.appendTo('.row');
     }
 }   
